0230779ea8b6ce4b4b5fea14e37fed30f1b115fb
Vasilis van Gemert <github@vasilis.nl>
2014-05-31 22:38:09 +0200

    Added the files

---
+++ crazy-vh.js
@@ -0,0 +1,29 @@
+// create two divs
+var a = document.createElement('div');
+var b = document.createElement('div');
+
+// Add them to the body
+document.documentElement.appendChild(a);
+document.documentElement.appendChild(b);
+
+// The first div must be higher than the viewport
+a.setAttribute('style','height:2000em');
+// The second div just as high
+b.setAttribute('style','height:100vh');
+
+// wrap it all in a timeout, or it won't work
+setTimeout(function(){
+    // Add something, ot it won't work
+    b.innerHTML = '1';
+    var dHeight = b.clientHeight;
+    var bHeight = window.innerHeight;
+
+    // If 100vh is higher than the viewport, vh is not supported well
+    if (dHeight > bHeight) {
+        // add a class the HTML element for alternative styling
+        document.documentElement.className += ' crazy-vh';
+    }
+    // Delete the elements
+    document.documentElement.removeChild(a);
+    document.documentElement.removeChild(b);
+},1);
